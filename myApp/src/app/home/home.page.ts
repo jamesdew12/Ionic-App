694009f3a6090a91894e3caae3b57887ead27ca8
Adrien Fabre <adrien.fabre.1@gmail.com>
2019-07-23 14:19:12 +0100

    Started to insert api

--- myApp/src/app/home/home.page.ts
+++ myApp/src/app/home/home.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import { HttpClient } from "@angular/common/http";
+import { NavController, NavParams } from "@ionic/angular";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  masters: Observable<any>;
 
-  constructor() {}
-
+  constructor(public navCtrl: NavController, public httpClient: HttpClient) {
+    this.masters = this.httpClient.get("https://swapi.co/api/");
+    this.masters.subscribe(data => {
+      console.log("my data: ", data);
+    });
+  }
 }
